dcdea906f671ed0b600fa3debcb6f6e20497a4da
Sorokin Eugene <RuAlastor@yandex.ru>
2019-04-08 15:36:40 +0300

    Fixed /string/ and finished /vector/

--- cpp_lang_task1.cpp
+++ cpp_lang_task1.cpp
@@ -1,52 +1,56 @@
 #include "cpp_lang_task1.h"
 
+using namespace Student;
+
+/* STRING */
+
 //
 /* Default destructor */
-Student::string::~string() {
+string::~string() {
     delete [] _data;
 }
 
 //
 /* Default constructor */
-Student::string::string() : _data(nullptr) { }
+string::string() : _data(nullptr), _size(0) { }
 /* Constructs a string with /count/ copies of /ch/*/
-Student::string::string(unsigned int count, char ch) {
+string::string(unsigned int count, char ch) {
     _data = new char[count + 1];
-    for (int i = 0; i < count; i++) {
+    for (unsigned int i = 0; i < count; i++) {
         _data[i] = ch;
     }
     _data[count] = '\0';
 }
 /* Constructs a string from a substring */
-Student::string::string(const char *other, unsigned int count) {
+string::string(const char *other, unsigned int count) {
     _data = new char[count];
     strncpy(_data, other, count);
 }
 /* Constructs a string from a char array
     Behaviour is undefined if strlen(other) is invalid*/
-Student::string::string(const char *other) {
+string::string(const char *other) {
     _data = new char[strlen(other)];
     strcpy(_data, other);
 }
 /* Copy constructor */
-Student::string::string(const string &other) : _data(other._data), _size(other._size) { }
+string::string(const string &other) : _data(other._data), _size(other._size) { }
 /* Move constructor */
-Student::string::string(string &&other) : _data(other._data), _size(other._size) {
+string::string(string &&other) : _data(other._data), _size(other._size) {
     other._data = nullptr;
     other._size = 0;
 }
 
 //
 /* Replaces contents with a copy */
-Student::string& Student::string::operator=(const string& other) {
+string& string::operator=(const string& other) {
     delete [] _data;
     _data = new char[other._size];
     strcpy(_data, other._data);
     _size = other._size;
-    return this;
+    return *this;
 }
 /* Replaces contents using move semantics */
-Student::string& Student::string::operator=(string&& other) {
+string& string::operator=(string&& other) {
     delete [] _data;
     _data = other._data;
     _size = other._size;
@@ -55,7 +59,7 @@ Student::string& Student::string::operator=(string&& other) {
     return *this;
 }
 /* Replaces contents with those of null-terminated character string */
-Student::string& Student::string::operator=(const char* other) {
+string& string::operator=(const char* other) {
     delete [] _data;
     _data = new char[strlen(other)];
     strcpy(_data, other);
@@ -65,70 +69,194 @@ Student::string& Student::string::operator=(const char* other) {
 
 //
 /* Returns a reference to the character at the specified /pos/ */
-char& Student::string::operator[](unsigned int pos) {
+char& string::operator[](unsigned int pos) {
     return _data[pos];
 }
-const char& Student::string::operator[](unsigned int pos) const {
+const char& string::operator[](unsigned int pos) const {
     return _data[pos];
 }
 /* Returns a reference to the first symbol */
-const char* Student::string::data() {
-    return _data;
+const char* string::data() const {
+    return this->_data;
 }
+/* Returns false if string is empty */
+bool string::empty() {
+    return static_cast<bool>(_size);
+}
+
 /* Returns length of the string */
-unsigned int Student::string::size() {
-    return _size;
+unsigned int string::size() const {
+    return this->_size;
 }
 /* Clears the contents */
-void Student::string::clear() {
+void string::clear() {
     delete [] _data;
     _data = nullptr;
     _size = 0;
 }
 /* Appends a character at the end */
-void Student::string::push_back(char ch) {
+void string::push_back(char ch) {
     strcat(_data, &ch);
 }
 /* Removes the last character */
-void Student::string::pop_back() {
+void string::pop_back() {
     char* buffer = new char[_size - 1];
     strncpy(buffer, _data, _size - 1);
     delete [] _data;
     _data = buffer;
-    buffer = nullptr;
 }
 
 //
 /* Appends characters /count/ symbols from /other/ */
-Student::string& Student::string::append(const char* other, unsigned int count) {
+string& string::append(const char* other, unsigned int count) {
     char* buffer = new char[_size + strlen(other)];
     strcpy(buffer, _data);
     delete [] _data;
     _data = buffer;
-    buffer = nullptr;
     strncat(_data, other, count);
     return *this;
 }
 /* Appends a string */
-Student::string& Student::string::append(const string &other) {
+string& string::append(const string &other) {
     char* buffer = new char[_size + other.size()];
     strcpy(buffer, _data);
     delete [] _data;
     _data = buffer;
-    buffer = nullptr;
-    strcat(_data, other);
+    strcat(_data, other._data);
+    return *this;
 }
 /* Appends a symbol /count/ times */
-Student::string& Student::string::append(unsigned int count, const char &ch) {
+string& string::append(unsigned int count, const char &ch) {
     char* buffer = new char[_size + sizeof(char) * count];
     strcpy(buffer, _data);
     delete [] _data;
     _data = buffer;
-    buffer = nullptr;
-    for (int i = 0; i < count; i++) {
+    for (unsigned int i = 0; i < count; i++) {
         strcat(_data, &ch);
     }
+    return *this;
 }
+
+
+/* VECTOR */
+
+//
+/* Default destructor */
+template <typename T>
+vector<T>::~vector() {
+    delete [] _data;
+}
+
+//
+/* Default constructor */
+template <typename T>
+vector<T>::vector() : _data(nullptr), _size(0) { }
+/* Copy constructor */
+template <typename T>
+vector<T>::vector(const vector &other) : _data(other._data), _size(other._size) { }
+/* Move constructor */
+template <typename T>
+vector<T>::vector(vector &&other) : _data(other._data), _size(other._size) {
+    other._data = nullptr;
+    other._size = 0;
+}
+/* Constructs a container with /count/ copies of /val/ */
+template <typename T>
+vector<T>::vector(unsigned int count, const T& val) : _size(count) {
+    _data = new T[_size];
+    for (unsigned int i = 0; i < count; i++) {
+        _data[i] = val;
+    }
+}
+/* Constructs a container with /count/ empty elements */
+template <typename T>
+vector<T>::vector(unsigned int count) : _size(count) {
+    _data = new T[_size];
+}
+
+//
+/* Returns a reference to the character at the speciified /pos/ */
+template <typename T>
+T& vector<T>::operator [](unsigned int pos) {
+    return _data[pos];
+}
+template <typename T>
+const T& vector<T>::operator [](unsigned int pos) const {
+    return _data[pos];
+}
+/* Returns pointer to the underlying array serving as elements storage */
+template <typename T>
+T* vector<T>::data() {
+    return _data;
+}
+template <typename T>
+const T* vector<T>::data() const {
+    return _data;
+}
+
+
+//
+/* Returns true if vector is empty */
+template <typename T>
+bool vector<T>::empty() const {
+    return static_cast<bool>(_size);
+}
+
+//
+/* Clears the contents */
+template <typename T>
+void vector<T>::clear() {
+    delete [] _data;
+    _data = nullptr;
+    _size = 0;
+}
+/* Appends an object to the end */
+template <typename T>
+void vector<T>::push_back(const T &value) {
+    T* buffer = new T[_size + 1];
+    memmove(buffer, _data, sizeof(T) * _size);
+    delete [] _data;
+    _data = buffer;
+    _data[_size++] = value;
+}
+/* Appends an object to the end using move semantic */
+template <typename T>
+void vector<T>::push_back(T&& value) {
+    T* buffer = new T[_size + 1];
+    memmove(buffer, _data, sizeof(T) * _size);
+    delete [] _data;
+    _data = buffer;
+    _data[_size++] = std::move(value);
+}
+/* Removes last element of the container */
+template <typename T>
+void vector<T>::pop_back() {
+    _size--;
+}
+/* Inserts /count/ copies of /val/ before /pos/ */
+template <typename T>
+void vector<T>::insert(unsigned int pos, unsigned int count, const T &value) {
+    T* buffer = new T[_size + count];
+    memmove(buffer, _data, sizeof(T) * pos);
+    memmove(buffer + pos + count, _data + pos, sizeof(T) * (_size - pos));
+    for (unsigned int i = pos; i < count; i++ ) {
+        buffer[i] = value;
+    }
+    _data = buffer;
+}
+
+
+/* magic */
+template class vector<int>;
+template class vector<char>;
+/* template class vector<double>;
+ * template class vector<float>;
+ * template class vector<std::string>;
+ * template class vector<Student::vector>;
+ *
+ * so on...
+*/
+
 
 
 
